1de7313f68872c79bbff322e21bd8f7f1fa298be
eun0leee <eun0leee@gmail.com>
2023-01-12 14:54:18 +0900

    feat: 퍼블리싱, scss 파일 추가

--- auth.js
+++ auth.js
@@ -1,3 +1,3 @@
-const CLIENT_ID = '111111111111111111';
+const REST_API_KEY = '60a459cce23fe4fec09599b7ffaac2dc';
 const REDIRECT_URI = 'http://localhost:1234';
-export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
